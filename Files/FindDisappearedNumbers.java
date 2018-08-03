7196f8979a74a8786fcba98b4087eef813a2fe1b
Will Smith <willdavsmith@gmail.com>
2018-08-03 14:15:03 -0500

    Updated FindDisappearedNumbers solution.

--- Files/FindDisappearedNumbers.java
+++ Files/FindDisappearedNumbers.java
@@ -1,20 +1,18 @@
 package Files;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.LinkedList;
 
 public class FindDisappearedNumbers {
    public static List<Integer> findDisappearedNumbers(int[] nums) {
-      List<Integer> l = new LinkedList<>();
-      for (int i=1; i<=nums.length; i++) {
-         l.add(i);
-      }
+      List<Integer> l = new ArrayList<>();
+      boolean[] b = new boolean[nums.length+1];
       for (int i=0; i<nums.length; i++) {
-         l.remove((Integer) nums[i]);
+         b[nums[i]] = true;
+
+      }
+      for (int i=1; i<b.length; i++) {
+         if (!b[i]) l.add(i);
       }
       return l;
-   }
-
-   public static void main(String[] args) {
-      System.out.println(findDisappearedNumbers(new int[]{}));
    }
 }
